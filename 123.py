813e91f3a03007297ef25fb3dc8159fe1c3a689f
knuser <wjdgkswh9507@naver.com>
2024-03-19 10:51:47 +0900

    push test

---
+++ 123.py
@@ -0,0 +1,9 @@
+
+sample = [1, 11, -22, -20, -50, -33, 0, -5, 12, 35, 19, -9, -1, -100, 99]
+negative_val = []
+
+for num in sample:
+    if 10< num < 50:
+        negative_val.append(num)
+
+print(negative_val)
